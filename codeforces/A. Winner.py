97c5d9f1ec52da1d9a5a7a51f338235a675805ef
Ken <WenShihKen@users.noreply.github.com>
2020-02-03 00:25:13 +0800

    new codeforces

---
+++ codeforces/A. Winner.py
@@ -0,0 +1,21 @@
+'''
+    Author: Ken
+    Date: 2020-02-02
+'''
+from collections import Counter
+
+n = int(input())
+all = Counter()
+temp = []
+
+for _ in range(n):
+    name, score = input().split()
+    all[name] += int(score)
+    temp.append((name,all[name]))
+
+max_value = max(all.values())
+
+for x,y in temp:
+    if y >= max_value and all[x] == max_value:
+        print(x)
+        break
